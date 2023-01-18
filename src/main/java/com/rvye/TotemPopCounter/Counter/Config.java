ca68419586c9b39805fdd6c28bf0a45e9902d8d6
rye <114079592+rvye@users.noreply.github.com>
2023-01-17 20:08:01 -0600

    buttonm

--- src/main/java/com/rvye/TotemPopCounter/Counter/Config.java
+++ src/main/java/com/rvye/TotemPopCounter/Counter/Config.java
@@ -1,8 +1,11 @@
 package com.rvye.TotemPopCounter.Counter;
-import com.rvye.TotemPopCounter.PopCounter;
+// import com.rvye.TotemPopCounter.PopCounter;
 
+import net.fabricmc.fabric.api.client.screen.v1.*;
+import net.minecraft.client.gui.screen.Screen;
+@SuppressWarnings("unused")
 public class Config {
 
+    Screen
 
-    PopCounter.LOGGER.info("N\nN\nN\nN\n")
 }
--- src/main/java/com/rvye/TotemPopCounter/PopCounter.java
+++ src/main/java/com/rvye/TotemPopCounter/PopCounter.java
@@ -3,6 +3,8 @@ package com.rvye.TotemPopCounter;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("unused")
 public class PopCounter implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("tpc");
 
